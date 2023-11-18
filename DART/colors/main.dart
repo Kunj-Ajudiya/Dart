03403476e4192de002d711abce652df0673c0cd9
Kunj Ajudiya <145858513+Kunj-Ajudiya@users.noreply.github.com>
2023-11-18 10:34:17 +0530

    Dart

---
+++ DART/colors/main.dart
@@ -0,0 +1,12 @@
+import 'black.dart';
+import 'green.dart';
+
+void main() {
+  var black_obj = Black();
+  black_obj.disp();
+  // Black().disp();
+
+  var green_obj = Green();
+  green_obj.disp();
+  // Green().disp()
+}
